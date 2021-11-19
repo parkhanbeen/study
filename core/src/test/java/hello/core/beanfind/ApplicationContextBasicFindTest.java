46eae6890dd049fa320b2cd9efbdf059d3a60719
박한빈 <phb0228@naver.com>
2021-11-19 21:41:31 +0900

    같은 타입의 빈일 경우의 테스트 예제를 추가하라

--- core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -3,14 +3,13 @@ package hello.core.beanfind;
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ApplicationContextBasicFindTest {
 
