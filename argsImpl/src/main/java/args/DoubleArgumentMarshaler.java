d5d6d73d2414743884be1b997d36693435fe0f43
박한빈 <phb0228@naver.com>
2021-11-27 15:13:11 +0900

    잘못 매핑된 타입을 수정하라

--- argsImpl/src/main/java/args/DoubleArgumentMarshaler.java
+++ argsImpl/src/main/java/args/DoubleArgumentMarshaler.java
@@ -12,11 +12,11 @@ public class DoubleArgumentMarshaler implements ArgumentMarshaler {
         String parameter = null;
         try {
             parameter = currentArgument.next();
-            doubleValue = Integer.parseInt(parameter);
+            doubleValue = Double.parseDouble(parameter);
         } catch (NoSuchElementException e) {
-            throw new ArgsException(ErrorCode.MISSING_INTEGER);
+            throw new ArgsException(ErrorCode.MISSING_DOUBLE);
         } catch (NumberFormatException e) {
-            throw new ArgsException(ErrorCode.INVALID_INTEGER, parameter);
+            throw new ArgsException(ErrorCode.INVALID_DOUBLE, parameter);
         }
     }
 
