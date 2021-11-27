972a30b0c15054faba1b3141bcfe6d75e9b3040e
박한빈 <phb0228@naver.com>
2021-11-27 16:37:53 +0900

    의존성 관계 주입시 opional일 경우의 예제를 추가하라

---
+++ core/src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,43 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void autowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+
+    }
+
+    static class TestBean {
+
+        /**
+         *  required = false 경우 의존 관계가 없으면 메서드 호출 자체가 안됨.
+         */
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+
+        }
+
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean1 = " + noBean2);
+
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean1 = " + noBean3);
+
+        }
+    }
+}
