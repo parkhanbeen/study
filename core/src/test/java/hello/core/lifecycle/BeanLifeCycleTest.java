18f67c0791400425cb10aeae95bbbf7259fbc50d
박한빈 <phb0228@naver.com>
2021-12-07 20:57:32 +0900

    @PostConstruct, @PreDestroy 예제를 추가하라

--- core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -21,7 +21,9 @@ public class BeanLifeCycleTest {
     static class LifeCycleConfig {
         // destroyMethod 의 defalut는 (inferred) 추론으로 되어있어 close나 shutdown같은 이름의 메소드를 자동으로 호출함.
         // (inferred)를 쓰기 싫을 경우 destroyMethod = ""으로 주면 됨.
-        @Bean(initMethod = "init", destroyMethod = "close")
+        // @Bean(initMethod = "init", destroyMethod = "close")
+
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring/dev");
--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -28,6 +31,7 @@ public class NetworkClient {
     }
 
     // 의존관계 설정 후 호출.
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
@@ -35,6 +39,7 @@ public class NetworkClient {
     }
 
     // 스프링 컨테이너 종료 후 호출.
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
