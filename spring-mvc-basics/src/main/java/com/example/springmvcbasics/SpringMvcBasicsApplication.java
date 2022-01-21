f17334acd9eba37a3e82fd4b1bbdf8efb3e04f49
phb0228 <phb0228@naver.com>
2022-01-21 15:56:28 +0900

    inital commit

---
+++ spring-mvc-basics/src/main/java/com/example/springmvcbasics/SpringMvcBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcBasicsApplication.class, args);
+	}
+
+}
---
+++ spring-mvc-basics/src/test/java/com/example/springmvcbasics/SpringMvcBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvcbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
