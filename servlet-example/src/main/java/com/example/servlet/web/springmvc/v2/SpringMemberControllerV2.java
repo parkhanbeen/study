5d3145ff15c8c433f6cee0a9a653e82fe5d4781d
phb0228 <phb0228@naver.com>
2022-01-21 15:17:12 +0900

    실용적으로 리펙토링하라

--- servlet-example/src/main/java/com/example/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ servlet-example/src/main/java/com/example/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -1,7 +1,8 @@
-package com.example.servlet.web.springmvc.v1;
+package com.example.servlet.web.springmvc.v2;
 
 import com.example.servlet.domain.member.Member;
 import com.example.servlet.domain.member.MemberRepository;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
@@ -9,12 +10,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class SpringMemberSaveControllerV1 {
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
 
   private MemberRepository memberRepository = MemberRepository.getInstance();
 
-  @RequestMapping("/springmvc/v1/members/save")
-  public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+  @RequestMapping("/new-form")
+  public ModelAndView newForm() {
+    return new ModelAndView("new-form");
+  }
+
+  @RequestMapping
+  public ModelAndView list() {
+
+    List<Member> members = memberRepository.findAll();
+
+    ModelAndView modelAndView = new ModelAndView("members");
+    modelAndView.addObject("members", members);
+
+    return modelAndView;
+  }
+
+  @RequestMapping("/members/save")
+  public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
     String username = request.getParameter("username");
     int age = Integer.parseInt(request.getParameter("age"));
 
