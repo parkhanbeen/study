66234bba5456da696ca9360a0b95729b4fa9af12
박한빈 <phb0228@naver.com>
2022-01-22 17:29:51 +0900

    ModelAttribute 예제를 추가하라

---
+++ spring-mvc-basics/src/main/java/com/example/springmvcbasics/basic/HelloData.java
@@ -0,0 +1,11 @@
+package com.example.springmvcbasics.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class HelloData {
+    private String username;
+    private int age;
+}
--- spring-mvc-basics/src/main/java/com/example/springmvcbasics/basic/request/RequestParamController.java
+++ spring-mvc-basics/src/main/java/com/example/springmvcbasics/basic/request/RequestParamController.java
@@ -1,6 +1,8 @@
 package com.example.springmvcbasics.basic.request;
 
+import com.example.springmvcbasics.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -74,6 +76,20 @@ public class RequestParamController {
     public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
 
         log.info("username = {}, age = {}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData = {}", helloData);
+        return "ok";
+    }
+
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData = {}", helloData);
         return "ok";
     }
 }
