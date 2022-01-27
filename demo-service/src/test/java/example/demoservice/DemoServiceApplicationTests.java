002335826f23e883ba88508a5c2a1824cc11293b
phb0228 <phb0228@naver.com>
2022-01-27 15:01:41 +0900

    intial commit

---
+++ demo-service/src/test/java/example/demoservice/DemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package example.demoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
