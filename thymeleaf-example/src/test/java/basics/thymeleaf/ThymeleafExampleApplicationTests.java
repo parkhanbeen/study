cb6c903efe455ce1eaf105c3f96304498e05d334
박한빈 <phb0228@naver.com>
2022-01-31 16:01:28 +0900

    initial commit

---
+++ thymeleaf-example/src/test/java/basics/thymeleaf/ThymeleafExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package basics.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
