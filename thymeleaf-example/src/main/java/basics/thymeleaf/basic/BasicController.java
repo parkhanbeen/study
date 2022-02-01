bbceee743103771af58e7800fb88fb6791c0f620
박한빈 <phb0228@naver.com>
2022-02-01 18:39:25 +0900

    속성값 설정 예제를 추가하라

--- thymeleaf-example/src/main/java/basics/thymeleaf/basic/BasicController.java
+++ thymeleaf-example/src/main/java/basics/thymeleaf/basic/BasicController.java
@@ -1,11 +1,14 @@
 package basics.thymeleaf.basic;
 
 import lombok.Getter;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -43,6 +46,52 @@ public class BasicController {
 
         return "basic/variable";
     }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "hello park");
+        return "basic/basic-objects";
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/local-date";
+    }
+
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!!");
+        return "basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "hello" + data;
+        }
+    }
+
 
     @Getter
     static class User {
