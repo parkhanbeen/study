5082b807dddaba5fff16154ace543c382fb9294d
박한빈 <phb0228@naver.com>
2022-02-19 16:16:40 +0900

    예외처리 예제 프로젝트를 생성하라

---
+++ exception-example/src/main/java/exception/example/ExceptionExampleApplication.java
@@ -0,0 +1,13 @@
+package exception.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExceptionExampleApplication.class, args);
+  }
+
+}
---
+++ exception-example/src/test/java/exception/example/ExceptionExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package exception.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
