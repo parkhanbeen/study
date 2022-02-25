11cb57add441a8ee65cbfe97d7caf0a96b0114d7
박한빈 <phb0228@naver.com>
2022-02-25 18:51:05 +0900

    inital commit

---
+++ multipart-example/src/main/java/com/multipart/example/MultipartApplication.java
@@ -0,0 +1,13 @@
+package com.multipart.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipartApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MultipartApplication.class, args);
+  }
+
+}
