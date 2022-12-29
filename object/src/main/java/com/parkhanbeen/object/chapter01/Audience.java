7aa89eed1819c7be447e03d11e56d5c02af7d94d
hanbeeeeeeeen <phb0228@naver.com>
2022-12-29 18:56:55 +0900

    Chapter 01 객체 설계
    * inital commit
    
    * refactor : 소극장에 모인 의존성을 제거
    
    * refactor : 관람객 캡슐화
    
    * doc : 절차지향과 객체지향 설명 작성
    
    * refector : bag의 결합도를 낮춰라
    
    * refector : Audience 결합도를 낮춰라

---
+++ object/src/main/java/com/parkhanbeen/object/chapter01/Audience.java
@@ -0,0 +1,17 @@
+package com.parkhanbeen.object.chapter01;
+
+/**
+ * 관람객.
+ */
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Long buy(Ticket ticket) {
+        return bag.hold(ticket);
+    }
+
+}
---
+++ object/src/main/java/com/parkhanbeen/object/chapter01/Invitation.java
@@ -0,0 +1,14 @@
+package com.parkhanbeen.object.chapter01;
+
+import java.time.LocalDateTime;
+
+/**
+ * 초대장.
+ */
+public class Invitation {
+
+    /**
+     * 초대 일자.
+     */
+    private LocalDateTime when;
+}
