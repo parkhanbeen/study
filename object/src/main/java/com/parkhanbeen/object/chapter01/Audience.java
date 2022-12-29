fc38a89c3fcd19a9a531c67f82e6c9c1df1cd2c1
박한빈/스타일콘텐츠프로덕트팀 <hanbeen.park@musinsa.com>
2022-12-29 17:14:01 +0900

    inital commit

---
+++ object/src/main/java/com/parkhanbeen/object/chapter01/Audience.java
@@ -0,0 +1,16 @@
+package com.parkhanbeen.object.chapter01;
+
+/**
+ * 관람객.
+ */
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
