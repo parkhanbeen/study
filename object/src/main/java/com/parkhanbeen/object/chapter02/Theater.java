140d8b12fa4feb6b7817861ad60f08f0adc4b273
박한빈/스타일콘텐츠프로덕트팀 <hanbeen.park@musinsa.com>
2022-12-30 17:37:35 +0900

    극장 클라이언트

--- object/src/main/java/com/parkhanbeen/object/chapter02/Theater.java
+++ object/src/main/java/com/parkhanbeen/object/chapter02/Theater.java
@@ -1,2 +1,32 @@
-package com.parkhanbeen.object.chapter02;public class Theater {
+package com.parkhanbeen.object.chapter02;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class Theater {
+
+    public void enter() {
+        Movie avatar = new Movie("아바타",
+                Duration.ofMinutes(120),
+                Money.wons(10000),
+                new AmountDiscountPolicy(Money.wons(800),
+                        new SequenceCondition(1),
+                        new SequenceCondition(10),
+                        new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59)),
+                        new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10, 0), LocalTime.of(20, 59))));
+
+        Movie titanic = new Movie("타이타닉",
+                Duration.ofMinutes(180),
+                Money.wons(11000),
+                new PercentDiscountPolicy(0.1,
+                        new PeriodCondition(DayOfWeek.TUESDAY, LocalTime.of(14, 0), LocalTime.of(16, 59)),
+                        new SequenceCondition(2),
+                        new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10, 0), LocalTime.of(13, 59))));
+
+        Movie starWars = new Movie("스타워즈",
+                Duration.ofMinutes(210),
+                Money.wons(10000),
+                new NoneDiscountPolicy());
+    }
 }
