49f75c893311c2acd016183cb10f50c5b622bbb4
박한빈/스타일콘텐츠프로덕트팀 <hanbeen.park@musinsa.com>
2023-01-02 19:16:19 +0900

    add-chapter-04

---
+++ object/src/main/java/com/parkhanbeen/object/chapter04/DiscountConditionType.java
@@ -0,0 +1,14 @@
+package com.parkhanbeen.object.chapter04;
+
+public enum DiscountConditionType {
+
+    /**
+     * 순번 조건.
+     */
+    SEQUENCE,
+
+    /**
+     * 기간 조건.
+     */
+    PERIOD,
+}
---
+++ object/src/main/java/com/parkhanbeen/object/chapter04/Movie.java
@@ -0,0 +1,61 @@
+package com.parkhanbeen.object.chapter04;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public Money calculateAmountDiscountedFee() {
+        if (movieType != MovieType.AMOUNT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(discountAmount);
+    }
+
+    public Money calculatePercentDiscountedFee() {
+        if (movieType != MovieType.PERCENT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(fee.times(discountPercent));
+    }
+
+    public Money calculateNoneDiscountedFee() {
+        if (movieType != MovieType.NONE_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee;
+    }
+
+    public boolean isDiscountable(LocalDateTime whenScreened, int sequence) {
+        for (DiscountCondition condition : discountConditions) {
+            if (condition.getType() == DiscountConditionType.PERIOD) {
+                if (condition.isDiscountable(whenScreened.getDayOfWeek(), whenScreened.toLocalTime())) {
+                    return true;
+                }
+            } else {
+                if (condition.isDiscountable(sequence)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
