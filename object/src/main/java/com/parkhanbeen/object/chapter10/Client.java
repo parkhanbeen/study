e5fcbef35db72a97b26be3ca7be473d3b22c951d
박한빈 <phb0228@naver.com>
2023-01-10 21:53:11 +0900

    clinet 작성

--- object/src/main/java/com/parkhanbeen/object/chapter10/Client.java
+++ object/src/main/java/com/parkhanbeen/object/chapter10/Client.java
@@ -1,2 +1,23 @@
-package com.parkhanbeen.object.chapter10;public class Client {
+package com.parkhanbeen.object.chapter10;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Client {
+
+  public void enter() {
+    Phone phone = new Phone(Money.wons(5), Duration.ofSeconds(10));
+
+    phone.call(new Call(
+        LocalDateTime.of(2018, 1, 1, 12, 10, 0),
+        LocalDateTime.of(2018, 1, 1, 12, 11, 0))
+    );
+
+    phone.call(new Call(
+        LocalDateTime.of(2018, 1, 2, 12, 10, 0),
+        LocalDateTime.of(2018, 1, 2, 12, 11, 0))
+    );
+
+    phone.calculateFee(); // Money.wons(60)
+  }
 }
