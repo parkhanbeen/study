b807ade01d605890fb31d0d002be518947081cbf
박한빈 <phb0228@naver.com>
2023-01-10 22:16:17 +0900

    타입 코드로 변경(안티 패턴)

--- object/src/main/java/com/parkhanbeen/object/chapter10/Phone.java
+++ object/src/main/java/com/parkhanbeen/object/chapter10/Phone.java
@@ -5,15 +5,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Phone {
+  private static final int LATE_NIGHT_HOUR = 22;
+
+  enum PhoneType {
+    REGULAR, NIGHTLY
+  }
+
+  private PhoneType type;
+
   private Money amount;
+  private Money nightlyAmount;
+  private Money regularAmount;
   private Duration seconds;
   private List<Call> calls = new ArrayList<>();
-  private double taxRate;
 
-  public Phone(Money amount, Duration seconds, double taxRate) {
+  public Phone(Money amount, Duration seconds) {
+    this(PhoneType.REGULAR, amount, Money.ZERO, Money.ZERO, seconds);
+  }
+
+  public Phone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+    this(PhoneType.NIGHTLY, Money.ZERO, nightlyAmount, regularAmount, seconds);
+  }
+
+  public Phone(PhoneType type,
+               Money amount,
+               Money nightlyAmount,
+               Money regularAmount,
+               Duration seconds) {
+    this.type = type;
     this.amount = amount;
+    this.nightlyAmount = nightlyAmount;
+    this.regularAmount = regularAmount;
     this.seconds = seconds;
-    this.taxRate = taxRate;
   }
 
   public void call(Call call) {
@@ -36,11 +59,22 @@ public class Phone {
     Money result = Money.ZERO;
 
     for (Call call : calls) {
-      result = result.plus(amount.times(
-          call.getDuration().getSeconds() / seconds.getSeconds())
-      );
+      if (type == PhoneType.REGULAR) {
+        result = result.plus(
+            amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+
+      } else {
+        if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
+          result = result.plus(
+              nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+        } else {
+          result = result.plus(
+              regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+
+        }
+      }
     }
 
-    return result.plus(result.times(taxRate));
+    return result;
   }
 }
