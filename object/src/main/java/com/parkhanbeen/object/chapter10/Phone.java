6a8e0c7b50a23e98c5d45556f147d5d2258b82b5
박한빈 <phb0228@naver.com>
2023-01-10 22:34:33 +0900

    상속을 이용해 중복 제거(상속의 문제점)

--- object/src/main/java/com/parkhanbeen/object/chapter10/Phone.java
+++ object/src/main/java/com/parkhanbeen/object/chapter10/Phone.java
@@ -5,37 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Phone {
-  private static final int LATE_NIGHT_HOUR = 22;
-
-  enum PhoneType {
-    REGULAR, NIGHTLY
-  }
-
-  private PhoneType type;
-
   private Money amount;
-  private Money nightlyAmount;
-  private Money regularAmount;
   private Duration seconds;
   private List<Call> calls = new ArrayList<>();
 
   public Phone(Money amount, Duration seconds) {
-    this(PhoneType.REGULAR, amount, Money.ZERO, Money.ZERO, seconds);
-  }
-
-  public Phone(Money nightlyAmount, Money regularAmount, Duration seconds) {
-    this(PhoneType.NIGHTLY, Money.ZERO, nightlyAmount, regularAmount, seconds);
-  }
-
-  public Phone(PhoneType type,
-               Money amount,
-               Money nightlyAmount,
-               Money regularAmount,
-               Duration seconds) {
-    this.type = type;
     this.amount = amount;
-    this.nightlyAmount = nightlyAmount;
-    this.regularAmount = regularAmount;
     this.seconds = seconds;
   }
 
@@ -59,20 +34,9 @@ public class Phone {
     Money result = Money.ZERO;
 
     for (Call call : calls) {
-      if (type == PhoneType.REGULAR) {
-        result = result.plus(
-            amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
-
-      } else {
-        if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
-          result = result.plus(
-              nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
-        } else {
-          result = result.plus(
-              regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
-
-        }
-      }
+      result = result.plus(amount.times(
+          call.getDuration().getSeconds() / seconds.getSeconds())
+      );
     }
 
     return result;
