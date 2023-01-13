cf62afba6ff745a42ccaa809acc9badd120bffb8
hanbeeeeeeeen <phb0228@naver.com>
2023-01-13 18:32:51 +0900

    chapter-10 상속과 코드 재사용

--- object/src/main/java/com/parkhanbeen/object/chapter10/NightlyDiscountPhone.java
+++ object/src/main/java/com/parkhanbeen/object/chapter10/NightlyDiscountPhone.java
@@ -1,35 +1,27 @@
 package com.parkhanbeen.object.chapter10;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 
 public class NightlyDiscountPhone extends Phone {
   private static final int LATE_NIGHT_HOUR = 22;
 
   private Money nightlyAmount;
+  private Money regularAmount;
+  private Duration seconds;
 
-  public NightlyDiscountPhone(Money amount, Duration seconds,
-                              Money nightlyAmount, double taxRate) {
-    super(amount, seconds, taxRate);
+  public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds, double taxRate) {
+    super(taxRate);
     this.nightlyAmount = nightlyAmount;
+    this.regularAmount = regularAmount;
+    this.seconds = seconds;
   }
 
   @Override
-  public Money calculateFee() {
-    // 부모 클래스의 calculateFee 호출
-    Money result = super.calculateFee();
-
-    Money nightlyFee = Money.ZERO;
-    for (Call call : getCalls()) {
-      if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
-        nightlyFee = nightlyFee.plus(
-            getAmount().minus(nightlyAmount).times(
-                call.getDuration().getSeconds() / getSeconds().getSeconds()));
-      }
+  protected Money calculateCallFee(Call call) {
+    if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
+      return nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds());
+    } else {
+      return regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds());
     }
-
-    // 중복된 코드를 수정하다 발생하는 이슈 minus -> plus
-    return result.minus(nightlyFee.plus(nightlyFee.times(getTaxRate())));
   }
 }
--- object/src/main/java/com/parkhanbeen/object/chapter10/Phone.java
+++ object/src/main/java/com/parkhanbeen/object/chapter10/Phone.java
@@ -1,50 +1,30 @@
 package com.parkhanbeen.object.chapter10;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Phone {
-  private Money amount;
-  private Duration seconds;
-  private List<Call> calls = new ArrayList<>();
-  private double taxRate;
+public abstract class Phone {
 
-  public Phone(Money amount, Duration seconds, double taxRate) {
-    this.amount = amount;
-    this.seconds = seconds;
-    this.taxRate = taxRate;
-  }
+    private double taxRate;
 
-  public void call(Call call) {
-    calls.add(call);
-  }
+    private List<Call> calls = new ArrayList<>();
 
-  public List<Call> getCalls() {
-    return calls;
-  }
-
-  public Money getAmount() {
-    return amount;
-  }
-
-  public Duration getSeconds() {
-    return seconds;
-  }
-
-  public double getTaxRate() {
-    return taxRate;
-  }
-
-  public Money calculateFee() {
-    Money result = Money.ZERO;
-
-    for (Call call : calls) {
-      result = result.plus(amount.times(
-          call.getDuration().getSeconds() / seconds.getSeconds())
-      );
+    public Phone(double taxRate) {
+        this.taxRate = taxRate;
     }
 
-    return result.plus(result.times(taxRate));
-  }
+    public Money calculateFee() {
+        // 부모 클래스의 calculateFee 호출
+        Money result = Money.ZERO;
+
+        for (Call call : calls) {
+            result = result.plus(
+                calculateCallFee(call));
+        }
+
+        return result;
+    }
+
+    abstract protected Money calculateCallFee(Call call);
+
 }
