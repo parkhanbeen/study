7f41e720f99e42e7a7ea3a59ca23c73f1dd2f1e1
hanbeeeeeeeen <phb0228@naver.com>
2023-01-20 17:04:27 +0900

    chapter 12 다형성

---
+++ object/src/main/java/com/parkhanbeen/object/chapter12/FormattedGradeLecture.java
@@ -0,0 +1,14 @@
+package com.parkhanbeen.object.chapter12;
+
+import java.util.List;
+
+public class FormattedGradeLecture extends GradeLecture {
+
+    public FormattedGradeLecture(String title, int pass, List<Integer> scores, List<Grade> grades) {
+        super(title, pass, scores, grades);
+    }
+
+    public String formatAverage() {
+        return String.format("Avg: %1.1f", super.average());
+    }
+}
