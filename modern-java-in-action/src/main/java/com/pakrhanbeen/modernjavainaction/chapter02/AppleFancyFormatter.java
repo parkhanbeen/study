1bd934cbe8595123b64274da60cd67fa512d49f3
박한빈/스타일콘텐츠프로덕트팀 <hanbeen.park@musinsa.com>
2023-02-12 19:41:15 +0900

    동작 파라미터화 코드 전달하기

---
+++ modern-java-in-action/src/main/java/com/pakrhanbeen/modernjavainaction/chapter02/AppleFancyFormatter.java
@@ -0,0 +1,10 @@
+package com.pakrhanbeen.modernjavainaction.chapter02;
+
+public class AppleFancyFormatter implements AppleFormatter {
+
+    @Override
+    public String accept(Apple apple) {
+        String characteristic = apple.getWeight() > 150 ? "heavy" : "light";
+        return "A " + characteristic + " " + apple.getColor() + " apple";
+    }
+}
---
+++ modern-java-in-action/src/main/java/com/pakrhanbeen/modernjavainaction/chapter02/AppleSimpleFormatter.java
@@ -0,0 +1,9 @@
+package com.pakrhanbeen.modernjavainaction.chapter02;
+
+public class AppleSimpleFormatter implements AppleFormatter {
+
+    @Override
+    public String accept(Apple apple) {
+        return "An apple of " + apple.getWeight() + "g";
+    }
+}
