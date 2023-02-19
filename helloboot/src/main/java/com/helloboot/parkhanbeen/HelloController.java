91bbedd0391fda4412b8afd5d54c5452b7ca6594
박한빈/스타일콘텐츠프로덕트팀 <hanbeen.park@musinsa.com>
2023-02-19 17:53:55 +0900

    spring boot 스탠드 얼론 예제

--- helloboot/src/main/java/com/helloboot/parkhanbeen/HelloController.java
+++ helloboot/src/main/java/com/helloboot/parkhanbeen/HelloController.java
@@ -1,13 +1,27 @@
 package com.helloboot.parkhanbeen;
 
+import java.util.Objects;
+
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private final HelloService helloService;
+
+    private final ApplicationContext applicationContext;
+
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+        this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println("applicationContext = " + applicationContext);
+    }
+
     @GetMapping("/hello")
     public String hello(String name) {
-        return "Hello " + name;
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 
 }
--- helloboot/src/main/java/com/helloboot/parkhanbeen/HellobootApplication.java
+++ helloboot/src/main/java/com/helloboot/parkhanbeen/HellobootApplication.java
@@ -1,47 +1,29 @@
 package com.helloboot.parkhanbeen;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
+@ComponentScan
 public class HellobootApplication {
 
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
     public static void main(String[] args) {
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-          servletContext.addServlet("frontController", new HttpServlet() {
-              @Override
-              protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-                  if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                      String name = req.getParameter("name");
-
-                      resp.setStatus(HttpStatus.OK.value());
-                      resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                      resp.getWriter().println("hello " + name);
-                  } else if (req.getRequestURI().equals("/user")) {
-                      resp.setStatus(HttpStatus.NOT_FOUND.value());
-
-                  } else {
-
-                  }
-
-
-              }
-          }).addMapping("/*");
-
-        });
-        webServer.start();
+        SpringApplication.run(HellobootApplication.class, args);
     }
 
 }
