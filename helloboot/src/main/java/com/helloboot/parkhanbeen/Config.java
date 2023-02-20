d8a3804569bb75578a3187c2034cebb0f8c7e0be
hanbeeeeeeeen <phb0228@naver.com>
2023-02-20 17:57:09 +0900

    커스텀 부트어플리케이션 어노테이션 추가

---
+++ helloboot/src/main/java/com/helloboot/parkhanbeen/Config.java
@@ -0,0 +1,22 @@
+package com.helloboot.parkhanbeen;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+}
