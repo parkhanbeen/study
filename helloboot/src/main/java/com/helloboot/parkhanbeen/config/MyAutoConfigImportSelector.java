7fd8d5f08ff60f2ca9724b881ff991edbd6df295
박한빈/스타일콘텐츠프로덕트팀 <hanbeen.park@musinsa.com>
2023-02-25 15:08:48 +0900

    외부 파일에서 config 등록 예제를 추가하라

--- helloboot/src/main/java/com/helloboot/parkhanbeen/config/MyAutoConfigImportSelector.java
+++ helloboot/src/main/java/com/helloboot/parkhanbeen/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,24 @@
 package com.helloboot.parkhanbeen.config;
 
+import java.util.stream.StreamSupport;
+
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-            "com.helloboot.parkhanbeen.config.autoconfig.DispatcherServletConfig",
-            "com.helloboot.parkhanbeen.config.autoconfig.TomcatWebserverConfig"
-        };
+        Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        return StreamSupport
+            .stream(candidates.spliterator(), false)
+            .toArray(String[]::new);
     }
+
 }
