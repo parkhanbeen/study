ed3033372e4e08801cf5ed7ed6be399c210c6c4b
hanbeeeeeeeen <phb0228@naver.com>
2023-03-11 15:22:47 +0900

    빈 후처리기 예제 추가

---
+++ helloboot/src/main/java/com/helloboot/parkhanbeen/config/autoconfig/ServerProperties.java
@@ -0,0 +1,27 @@
+package com.helloboot.parkhanbeen.config.autoconfig;
+
+import com.helloboot.parkhanbeen.config.MyConfigurationProperties;
+
+@MyConfigurationProperties(prefix = "server")
+public class ServerProperties {
+
+    private String contextPath;
+
+    private int port;
+
+    public String getContextPath() {
+        return contextPath;
+    }
+
+    public void setContextPath(String contextPath) {
+        this.contextPath = contextPath;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
--- helloboot/src/main/java/com/helloboot/parkhanbeen/config/autoconfig/TomcatWebserverConfig.java
+++ helloboot/src/main/java/com/helloboot/parkhanbeen/config/autoconfig/TomcatWebserverConfig.java
@@ -1,24 +1,28 @@
 package com.helloboot.parkhanbeen.config.autoconfig;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.helloboot.parkhanbeen.config.EnableMyConfigurationProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import com.helloboot.parkhanbeen.config.ConditionalMyOnClass;
 import com.helloboot.parkhanbeen.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
+@EnableMyConfigurationProperties(ServerProperties.class)
 public class TomcatWebserverConfig {
-    @Value("${contextPath}")
-    String contextPath;
 
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory serverFactory() {
+    public ServletWebServerFactory serverFactory(ServerProperties properties) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(this.contextPath);
+
+        factory.setContextPath(properties.getContextPath());
+        factory.setPort(properties.getPort());
+
         return factory;
     }
+
 }
