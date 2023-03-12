c5d08f379085cdd02039fef9ead99a075d39ebcb
박한빈/스타일콘텐츠프로덕트팀 <hanbeen.park@musinsa.com>
2023-03-12 19:18:49 +0900

    자동 구성 파악 예제

---
+++ springboot-auto/src/test/java/com/example/springbootauto/SpringbootAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
