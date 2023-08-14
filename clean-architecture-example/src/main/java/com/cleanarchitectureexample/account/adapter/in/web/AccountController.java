d4db9b7358fa06563d79b44deef5284ec41369e5
hanbeeeeeeeen <phb0228@naver.com>
2023-08-14 17:26:01 +0900

    chapter 5

--- clean-architecture-example/src/main/java/com/cleanarchitectureexample/account/adapter/in/web/AccountController.java
+++
@@ -1,5 +0,0 @@
-package com.cleanarchitectureexample.account.adapter.in.web;
-
-class AccountController {
-
-}
--- clean-architecture-example/src/main/java/com/cleanarchitectureexample/account/domain/AccountId.java
+++ clean-architecture-example/src/main/java/com/cleanarchitectureexample/account/domain/AccountId.java
@@ -2,4 +2,9 @@ package com.cleanarchitectureexample.account.domain;
 
 public class AccountId {
 
+    private final Long accountId;
+
+    public AccountId(Long accountId) {
+        this.accountId = accountId;
+    }
 }
