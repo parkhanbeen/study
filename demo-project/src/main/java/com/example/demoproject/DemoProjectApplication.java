c30499b67bb69de58be2ba016b0622a763d65d96
parkhanbeen <hanbeen.park@musinsa.com>
2023-09-27 17:50:17 +0900

    pid 파일 설정 추가

--- demo-project/src/main/java/com/example/demoproject/DemoProjectApplication.java
+++ demo-project/src/main/java/com/example/demoproject/DemoProjectApplication.java
@@ -2,12 +2,15 @@ package com.example.demoproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class DemoProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoProjectApplication.class, args);
+        SpringApplication application = new SpringApplication(DemoProjectApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 
 }
