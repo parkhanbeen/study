30b21a9458e2833b95ca52c502eb5f615f944254
parkhanbeen <hanbeen.park@musinsa.com>
2023-09-27 15:23:02 +0900

    빌드 테스트

--- demo-project/src/test/java/com/example/demoproject/DemoProjectApplicationTests.java
+++ demo-project/src/test/java/com/example/demoproject/DemoProjectApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demoproject;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class DemoProjectApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertThat(true).isTrue();
     }
 
 }
